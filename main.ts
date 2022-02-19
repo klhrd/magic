18137d380ec0752810d97a6250f8ff591d6008f3
klhrd <93413479+klhrd@users.noreply.github.com>
2022-02-19 09:04:56 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+let poker: number[] = []
+function 撲克牌洗牌_跳過 (跳過の數: number) {
+    poker = []
+    for (let index = 0; index <= 11; index++) {
+        for (let index2 = 0; index2 < 4; index2++) {
+        	
+        }
+    }
+}
+basic.forever(function () {
+	
+})
