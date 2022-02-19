27064d6bc384bf52833179938f968aed1e4a3055
klhrd <93413479+klhrd@users.noreply.github.com>
2022-02-19 09:16:53 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 let poker: number[] = []
 function 撲克牌洗牌_跳過 (跳過の數: number) {
     poker = []
-    for (let index = 0; index <= 11; index++) {
+    for (let index = 0; index <= 12; index++) {
         for (let index2 = 0; index2 < 4; index2++) {
-        	
+            poker.insertAt(randint(0, 0 - poker.length), index + 1)
         }
+    }
+    for (let index = 0; index < 4; index++) {
+        poker.removeAt(poker.indexOf(跳過の數))
     }
 }
 basic.forever(function () {
